f91306b406475fa668e826361a0f89a43e3125d0
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2023-05-10 22:53:22 -0400

    balls hehehe

--- vuework/src/components/getAPI.vue
+++ vuework/src/components/getAPI.vue
@@ -1,6 +1,8 @@
 <script>
+async function name(){
 let link = "https://data.cityofnewyork.us/resource/fcau-jc6k.json"
 let response = await fetch(link)
-let data_ = await response.json()
-export let dataBIG = data_
+return await response.json()
+}
+export let dataBIG = name
 </script>
--- vuework/src/components/pieChart.vue
+++ vuework/src/components/pieChart.vue
@@ -11,11 +11,17 @@ import {
   Responsive,
 } from "vue3-charts";
 import { dataBIG } from "./getAPI.vue";
-let prop = defineProps(['year'])
-let data = dataBIG.filter(s=> s.year== (prop.year||"2019")).map(s=> {s.race = String(s.materal_race_or_ethnicity || s.race); delete s.materal_race_or_ethnicity; return s})
+let prop = defineProps(['year']), data, loaded = ref(false)
+async function name(){
+  let dataBIG2 = await dataBIG()
+  data = dataBIG2.filter(s=> s.year== (prop.year||"2019")).map(s=> {s.race = String(s.materal_race_or_ethnicity || s.race); delete s.materal_race_or_ethnicity; return s})
+  ref.value = true
+}
+name()
 </script>
 <template>
 <Chart
+v-if="loaded"
 :size="{ width:420, height: 420 }"
 :data="data"
 :margin="{
