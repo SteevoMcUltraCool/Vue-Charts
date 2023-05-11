d79b8f2d2de371de16083d1d3f60db8366a57c32
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-05-11 10:12:19 -0400

    Update barGraph.vue

--- vuework/src/components/barGraph.vue
+++ vuework/src/components/barGraph.vue
@@ -11,13 +11,11 @@ import {
   Responsive,
 } from "vue3-charts";
 import { dataBIG } from "./getAPI.vue";
-let prop = defineProps(['year']), data,loaded = ref(false)
-async function name(){
-let dataBIG2 = await dataBIG()
- data = dataBIG2.filter(s=> s.year== (prop.year||"2019")).map(s=> {s.race = String(s.materal_race_or_ethnicity ||s.race); delete s.materal_race_or_ethnicity; return s})
- console.log(data) 
+let prop = defineProps(['year','data'])
+let data = prop.data.filter(s=> s.year== (prop.year||"2019")).map(s=> {s.race = String(s.materal_race_or_ethnicity ||s.race); delete s.materal_race_or_ethnicity; return s})
+console.log(data)
 loaded.value = true
-}
+
 name()
 </script>
 <template>
